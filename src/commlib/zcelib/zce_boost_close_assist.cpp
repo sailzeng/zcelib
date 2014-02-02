b470dbfcbceb79e0b8f46bd63f0ba1cd3d54e64e
sailzeng <fullsail@163.com>
2014-02-03 00:16:20 +0800

    Linux 下对于名字空间编译有问题，尝试修正。

--- src/commlib/zcelib/zce_boost_close_assist.cpp
+++ src/commlib/zcelib/zce_boost_close_assist.cpp
@@ -3,18 +3,21 @@
 #include "zce_os_adapt_file.h"
 #include "zce_boost_close_assist.h"
 
+namespace ZCE_LIB
+{
 
-template <>
-ZCE_LIB::close_assist<FILE *>::~close_assist()
+
+template <> close_assist<FILE *>::~close_assist()
 {
     ::fclose(to_close_);
 }
 
 
-template <>
-ZCE_LIB::close_assist<ZCE_HANDLE>::~close_assist()
+
+template <> close_assist<ZCE_HANDLE>::~close_assist()
 {
     ZCE_OS::close(to_close_);
 }
 
 
+}
