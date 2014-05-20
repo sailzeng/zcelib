fded118cde56bb2f1eb4593e65882517fcdd7c6d
sailzeng <fullsail@163.com>
2014-05-20 19:10:08 +0800

    lua的代码开始写了。

--- src/commlib/zcelib/zce_script_lua_tie.cpp
+++ src/commlib/zcelib/zce_script_lua_tie.cpp
@@ -5,6 +5,11 @@
 
 #if defined  lua_h
 
+
+
+
+
+
 ZCE_Lua_Tie::ZCE_Lua_Tie() :
     lua_state_(nullptr)
 {
@@ -44,52 +49,251 @@ void ZCE_Lua_Tie::close()
 }
 
 
+//=======================================================================================================
+//read_stack从堆栈中读取一个数据
+
+template<> char *ZCE_Lua_Tie::read_stack(lua_State *lua_state, int index)
+{
+    return (char *)lua_tostring(lua_state, index);
+}
+
+template<> const char *ZCE_Lua_Tie::read_stack(lua_State *lua_state, int index)
+{
+    return (const char *)lua_tostring(lua_state, index);
+}
+
+template<> char ZCE_Lua_Tie::read_stack(lua_State *lua_state, int index)
+{
+    return (char)lua_tonumber(lua_state, index);
+}
+
+template<> unsigned char ZCE_Lua_Tie::read_stack(lua_State *lua_state, int index)
+{
+    return (unsigned char)lua_tonumber(lua_state, index);
+}
+
+template<> short ZCE_Lua_Tie::read_stack(lua_State *lua_state, int index)
+{
+    return (short)lua_tonumber(lua_state, index);
+}
+
+template<> unsigned short ZCE_Lua_Tie::read_stack(lua_State *lua_state, int index)
+{
+    return (unsigned short)lua_tonumber(lua_state, index);
+}
+
+template<> int ZCE_Lua_Tie::read_stack(lua_State *lua_state, int index)
+{
+    return (int)lua_tonumber(lua_state, index);
+}
+
+template<> unsigned int ZCE_Lua_Tie::read_stack(lua_State *lua_state, int index)
+{
+    return (unsigned int)lua_tonumber(lua_state, index);
+}
+
+template<> float ZCE_Lua_Tie::read_stack(lua_State *lua_state, int index)
+{
+    return (float)lua_tonumber(lua_state, index);
+}
+
+template<> double ZCE_Lua_Tie::read_stack(lua_State *lua_state, int index)
+{
+    return (double)lua_tonumber(lua_state, index);
+}
+
+template<> bool ZCE_Lua_Tie::read_stack(lua_State *lua_state, int index)
+{
+    if (lua_isboolean(lua_state, index))
+    {
+        return lua_toboolean(lua_state, index) != 0;
+    }
+    //避免某些程度的转换不成功？
+    else
+    {
+        return lua_tonumber(lua_state, index) != 0;
+    }
+}
+
+template<> int64_t ZCE_Lua_Tie::read_stack(lua_State *lua_state, int index)
+{
+    if (lua_isnumber(lua_state, index))
+    {
+        return (int64_t)lua_tonumber(lua_state, index);
+    }
+    else
+    {
+        return *(int64_t *)lua_touserdata(lua_state, index);
+    }
+}
+template<> uint64_t ZCE_Lua_Tie::read_stack(lua_State *lua_state, int index)
+{
+    if (lua_isnumber(lua_state, index))
+    {
+        return (uint64_t)lua_tonumber(lua_state, index);
+    }
+    else
+    {
+        return *(uint64_t *)lua_touserdata(lua_state, index);
+    }
+}
+
+//=======================================================================================================
+//read_stack从堆栈中读取一个数据
+template<> void ZCE_Lua_Tie::push_stack(lua_State *lua_state, char val)
+{
+    lua_pushnumber(lua_state, val);
+}
+
+template<> void ZCE_Lua_Tie::push_stack(lua_State *lua_state, unsigned char val)
+{
+    lua_pushnumber(lua_state, val);
+}
+
+template<> void ZCE_Lua_Tie::push_stack(lua_State *lua_state, short val)
+{
+    lua_pushnumber(lua_state, val);
+}
+
+template<> void ZCE_Lua_Tie::push_stack(lua_State *lua_state, unsigned short val)
+{
+    lua_pushnumber(lua_state, val);
+}
+
+template<> void ZCE_Lua_Tie::push_stack(lua_State *lua_state, long val)
+{
+    lua_pushnumber(lua_state, val);
+}
+
+template<>
+void ZCE_Lua_Tie::push_stack(lua_State *lua_state, unsigned long val)
+{
+    lua_pushnumber(lua_state, val);
+}
+
+template<> void ZCE_Lua_Tie::push_stack(lua_State *lua_state, int val)
+{
+    lua_pushnumber(lua_state, val);
+}
+
+template<> void ZCE_Lua_Tie::push_stack(lua_State *lua_state, unsigned int val)
+{
+    lua_pushnumber(lua_state, val);
+}
+
+template<> void ZCE_Lua_Tie::push_stack(lua_State *lua_state, float val)
+{
+    lua_pushnumber(lua_state, val);
+}
+
+template<> void ZCE_Lua_Tie::push_stack(lua_State *lua_state, double val)
+{
+    lua_pushnumber(lua_state, val);
+}
+
+template<>
+void ZCE_Lua_Tie::push_stack(lua_State *lua_state, char *val)
+{
+    lua_pushstring(lua_state, val);
+}
+
+template<>
+void ZCE_Lua_Tie::push_stack(lua_State *lua_state, const char *val)
+{
+    lua_pushstring(lua_state, val);
+}
+
+template<> void ZCE_Lua_Tie::push_stack(lua_State *lua_state, bool val)
+{
+    lua_pushboolean(lua_state, val);
+}
+
+template<> void ZCE_Lua_Tie::push_stack(lua_State *lua_state, int64_t val)
+{
+    *(int64_t *)lua_newuserdata(lua_state, sizeof(int64_t)) = val;
+    lua_pushstring(lua_state, "int64_t");
+    lua_gettable(lua_state, LUA_GLOBALSINDEX);
+    lua_setmetatable(lua_state, -2);
+}
+template<> void ZCE_Lua_Tie::push_stack(lua_State *lua_state, int64_t val)
+{
+    *(uint64_t *)lua_newuserdata(lua_state, sizeof(uint64_t)) = val;
+    lua_pushstring(lua_state, "uint64_t");
+    lua_gettable(lua_state, LUA_GLOBALSINDEX);
+    lua_setmetatable(lua_state, -2);
+}
+
 
 //=======================================================================================================
 //为int64_t 准备的metatable
 static int tostring_int64(lua_State *lua_state)
 {
     char temp[64];
-    snprintf(temp, 63, "%lld", *(int64_t *)lua_topointer(lua_state, 1));
+    snprintf(temp, 63, "%lld", *(int64_t *)lua_touserdata(lua_state, 1));
     lua_pushstring(lua_state, temp);
     return 1;
 }
 
 static int eq_int64(lua_State *lua_state)
 {
-    int64_t a = *(int64_t *)lua_topointer(lua_state, 1);
-    int64_t b = *(int64_t *)lua_topointer(lua_state, 2);
+    int64_t a = *(int64_t *)lua_touserdata(lua_state, 1);
+    int64_t b = *(int64_t *)lua_touserdata(lua_state, 2);
     lua_pushboolean(lua_state, (a==b));
     return 1;
 }
 
 static int lt_int64(lua_State *lua_state)
 {
-    int64_t a = *(int64_t *)lua_topointer(lua_state, 1);
-    int64_t b = *(int64_t *)lua_topointer(lua_state, 2);
+    int64_t a = *(int64_t *)lua_touserdata(lua_state, 1);
+    int64_t b = *(int64_t *)lua_touserdata(lua_state, 2);
     lua_pushboolean(lua_state, (a < b));
     return 1;
 }
 
 static int le_int64(lua_State *lua_state)
 {
-    int64_t a = *(int64_t *)lua_topointer(lua_state, 1);
-    int64_t b = *(int64_t *)lua_topointer(lua_state, 2);
+    int64_t a = *(int64_t *)lua_touserdata(lua_state, 1);
+    int64_t b = *(int64_t *)lua_touserdata(lua_state, 2);
     lua_pushboolean(lua_state, (a <= b));
+    return 1;
+}
+
+static int add_int64(lua_State *lua_state)
+{
+    int64_t a = *(int64_t *)lua_touserdata(lua_state, 1);
+    int64_t b = *(int64_t *)lua_touserdata(lua_state, 2);
+    int64_t c = a + b;
+    ZCE_Lua_Tie::push_stack(c);
+    return 1;
+}
+
+static int sub_int64(lua_State *lua_state)
+{
+    int64_t a = *(int64_t *)lua_touserdata(lua_state, 1);
+    int64_t b = *(int64_t *)lua_touserdata(lua_state, 2);
+    int64_t c = a - b;
+    ZCE_Lua_Tie::push_stack(c);
     return 1;
 }
 
 static int set_int64(lua_State *lua_state)
 {
-    int64_t *data = (int64_t *)lua_topointer(lua_state, 1);
-    sscanf(lua_tostring(lua_state, 2),"%lld",data);
-    return 1;
+    int64_t *data = (int64_t *)lua_touserdata(lua_state, 1);
+    sscanf(lua_tostring(lua_state, 2), "%lld", data);
+    return 0;
 }
 
 static int selfadd_int64(lua_State *lua_state)
 {
-    int64_t *data = (int64_t *)lua_topointer(lua_state, 1);
+    int64_t *data = (int64_t *)lua_touserdata(lua_state, 1);
     ++(*data);
+    return 1;
+}
+
+static int selfsub_int64(lua_State *lua_state)
+{
+    int64_t *data = (int64_t *)lua_touserdata(lua_state, 1);
+    --(*data);
     return 1;
 }
 
@@ -128,15 +332,15 @@ void ZCE_Lua_Tie::register_int64(lua_State *lua_state)
 static int tostring_uint64(lua_State *lua_state)
 {
     char temp[64];
-    snprintf(temp,63, "%llu", *(uint64_t *)lua_topointer(lua_state, 1));
+    snprintf(temp,63, "%llu", *(uint64_t *)lua_touserdata(lua_state, 1));
     lua_pushstring(lua_state, temp);
     return 1;
 }
 
 static int eq_uint64(lua_State *lua_state)
 {
-    bool bret = (memcmp(lua_topointer(lua_state, 1),
-        lua_topointer(lua_state, 2),
+    bool bret = (memcmp(lua_touserdata(lua_state, 1),
+        lua_touserdata(lua_state, 2),
         sizeof(unsigned long long)) == 0);
     lua_pushboolean(lua_state, bret);
     return 1;
@@ -144,8 +348,8 @@ static int eq_uint64(lua_State *lua_state)
 
 static int lt_uint64(lua_State *lua_state)
 {
-    bool bret = (memcmp(lua_topointer(lua_state, 1),
-        lua_topointer(lua_state, 2),
+    bool bret = (memcmp(lua_touserdata(lua_state, 1),
+        lua_touserdata(lua_state, 2),
         sizeof(unsigned long long)) < 0);
     lua_pushboolean(lua_state, bret);
     return 1;
@@ -153,8 +357,8 @@ static int lt_uint64(lua_State *lua_state)
 
 static int le_uint64(lua_State *lua_state)
 {
-    bool bret = (memcmp(lua_topointer(lua_state, 1),
-        lua_topointer(lua_state, 2),
+    bool bret = (memcmp(lua_touserdata(lua_state, 1),
+        lua_touserdata(lua_state, 2),
         sizeof(unsigned long long)) <= 0);
     lua_pushboolean(lua_state, bret);
     return 1;
--- src/commlib/zcelib/zce_script_lua_tie.h
+++ src/commlib/zcelib/zce_script_lua_tie.h
@@ -5,6 +5,17 @@
 
 #if defined  lua_h
 
+
+
+namespace ZCE_LIB
+{ 
+
+
+
+
+};
+
+
 //LUA 鞋带，用于帮助绑定C++和
 class ZCE_Lua_Tie
 {
@@ -21,6 +32,39 @@ public:
     void register_int64(lua_State *lua_state);
     //
     void register_uint64(lua_State *lua_state);
+
+protected:
+
+    //不允许出现long的变量，因为long无法移植，
+
+    ///PUSH一个数据到堆栈，
+    static template<>  void push_stack(lua_State *lua_state, char ret);
+    static template<>  void push_stack(lua_State *lua_state, unsigned char ret);
+    static template<>  void push_stack(lua_State *lua_state, short ret);
+    static template<>  void push_stack(lua_State *lua_state, unsigned short ret);
+    static template<>  void push_stack(lua_State *lua_state, int ret);
+    static template<>  void push_stack(lua_State *lua_state, unsigned int ret);
+    static template<>  void push_stack(lua_State *lua_state, float ret);
+    static template<>  void push_stack(lua_State *lua_state, double ret);
+    static template<>  void push_stack(lua_State *lua_state, char *ret);
+    static template<>  void push_stack(lua_State *lua_state, const char *ret);
+    static template<>  void push_stack(lua_State *lua_state, bool ret);
+    static template<>  void push_stack(lua_State *lua_state, int64_t ret);
+    static template<>  void push_stack(lua_State *lua_state, uint64_t ret);
+
+    static template<>  char               *read_stack(lua_State *lua_state, int index);
+    static template<>  const char         *read_stack(lua_State *lua_state, int index);
+    static template<>  char                read_stack(lua_State *lua_state, int index);
+    static template<>  unsigned char       read_stack(lua_State *lua_state, int index);
+    static template<>  short               read_stack(lua_State *lua_state, int index);
+    static template<>  unsigned short      read_stack(lua_State *lua_state, int index);
+    static template<>  int                 read_stack(lua_State *lua_state, int index);
+    static template<>  unsigned int        read_stack(lua_State *lua_state, int index);
+    static template<>  float               read_stack(lua_State *lua_state, int index);
+    static template<>  double              read_stack(lua_State *lua_state, int index);
+    static template<>  bool                read_stack(lua_State *lua_state, int index);
+    static template<>  int64_t             read_stack(lua_State *lua_state, int index);
+    static template<>  uint64_t            read_stack(lua_State *lua_state, int index);
 
 protected:
 
