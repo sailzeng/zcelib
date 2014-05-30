3db1b72fdc6f470de72a8c7637614cfd9890c132
sailzeng <fullsail@163.com>
2014-05-30 22:00:42 +0800

    提交一个空文件。

---
+++ src/commlib/zcelib/zce_boost_any.h
@@ -0,0 +1,18 @@
+/*!
+* @copyright  2004-2013  Apache License, Version 2.0 FULLSAIL
+* @filename   zce_boost_any.h
+* @author     Sailzeng <sailerzeng@gmail.com>
+* @version
+* @date       2014年5月30日
+* @brief      
+* 
+* @details
+* 
+* @note       蔡英文：现在的台湾，没有一个人会认为政府有权力用武力镇压人民，
+*             也没有一个人会认为批评政府可以被暴力消音。这一条路，我们走得很
+*             漫长，走得很辛苦。今天我们在台湾追思“二.二八”，我相信有一天民
+*             主的中国也会用同样慎重的心情来追思那个日子。台湾能，大陆没有理
+*             由不能！
+*/
+
+
