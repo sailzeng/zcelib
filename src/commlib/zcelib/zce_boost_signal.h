47eb7c9cb3714cc4c947c1bfd029607b84bcdf4a
sailzeng <fullsail@163.com>
2015-03-11 10:12:55 +0800

    增加一些注释的文件。

--- src/commlib/zcelib/zce_boost_signal.h
+++ src/commlib/zcelib/zce_boost_signal.h
@@ -1,4 +1,15 @@
+/*
+Notepad++的作者在Notepad++ 6.7.4版本又做了一件很叼的事情：
+Notepad++自己打出这样一段话。
 
+Freedom of expression is like the air we breathe, we don't feel it, until people take it away from us.
+
+For this reason, Je suis Charlie, not because I endorse everything they published, but because I cherish the right to speak out freely without risk even when it offends others.
+And no, you cannot just take someone's life for whatever he/she expressed.
+
+Hence this "Je suis Charlie" edition.
+- #JeSuisCharlie
+*/
 
 #ifndef ZCE_LIB_BOOST_SIGNAL_H_
 #define ZCE_LIB_BOOST_SIGNAL_H_
