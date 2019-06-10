fa98d481c7cefe6cfd4c1fe039f2d5d1e3bce6ec
fullsail <fullsail@163.com>
2019-06-11 01:53:54 +0800

    去掉一部分无用注释。

--- src/commlib/zcelib/zce_os_adapt_getopt.cpp
+++ src/commlib/zcelib/zce_os_adapt_getopt.cpp
@@ -1,19 +1,3 @@
-/******************************************************************************************
-Copyright           : 2002-2003, FXL Technology (Shenzhen) Company Limited.
-FileName            : zce_os_adapt_layer.h
-Author              : Sail (ZENGXING)/Author name here
-Version             :
-Date Of Creation    : 2011-10-20
-Description         :
-
-Others              :
-Function List       :
-1.  ......
-Modification History:
-1.Date  :
-Author  :
-Modification  :
-******************************************************************************************/
 #include "zce_predefine.h"
 #include "zce_os_adapt_predefine.h"
 #include "zce_os_adapt_math.h"
