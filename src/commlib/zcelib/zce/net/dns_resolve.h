e955b0a49f2a00e97dacf8a6bd1d063051f05028
sailzeng <fullsail@163.com>
2021-10-27 02:44:14 +0800

    DNS查询， 兼容in和in6的地址类。

---
+++ src/commlib/zcelib/zce/net/dns_resolve.h
@@ -0,0 +1,101 @@
+﻿/*!
+* @copyright  2004-2021  Apache License, Version 2.0 FULLSAIL
+* @filename   zce/net/dns_resolve.h
+* @author     Sailzeng <sailzeng.cn@gmail.com>
+* @version
+* @date       nim1z
+* @brief
+* @details
+*
+*
+*
+* @note
+*
+*/
+
+#pragma once
+
+#include "zce/os_adapt/socket.h"
+#include "zce/socket/addr_any.h"
+#include "zce/socket/datagram.h"
+
+namespace zce
+{
+class DNS_Resolve
+{
+public:
+
+    DNS_Resolve();
+    ~DNS_Resolve();
+
+    int initialize(sockaddr *dns_svr_addr,
+                   socklen_t addr_len);
+
+    int initialize(int dns_svr_family,
+                   const char *dns_svr_ip,
+                   uint16_t dns_svr_port);
+
+    int add_dns_server(int dns_svr_family,
+                       const char *dns_svr_ip,
+                       uint16_t dns_svr_port);
+
+    ZCE_SOCKET get_handle();
+
+    //发起域名查询
+    int query(const char *query_name,
+              uint16_t query_type,
+              uint16_t *tid);
+
+    //
+    int answer(uint16_t *tid,
+               int family,
+               struct sockaddr addrs[],
+               size_t *addrs_num,
+               zce::Time_Value* timeout_tv);
+
+protected:
+
+    static int pack_request(char *buf,
+                            size_t *size,
+                            const char *query_name,
+                            uint16_t query_type,
+                            uint16_t *tid);
+
+    static int parse_response(char *buf,
+                              size_t size,
+                              uint16_t *tid,
+                              int family,
+                              struct sockaddr addrs[],
+                              size_t *addrs_num);
+
+public:
+
+    //查询类型，目前只支持这两种A 和AAAA
+    static constexpr uint16_t QTYPE_A = 1;
+    static constexpr uint16_t QTYPE_AAAA = 28;
+    //查询类，一般都是1
+    static constexpr uint16_t QCLASS_IN = 1;
+
+protected:
+    //DNS 包的最大长度
+    static constexpr size_t MAX_PACKET_LEN = 512;
+    //DNS 头部长度
+    static constexpr size_t DNS_HEADER_LEN = 12;
+    //每个标签最大的长度
+    static constexpr size_t MAX_LABEL_LEN = 63;
+    //查询名称的最大长度
+    static constexpr size_t MAX_NAME_LEN = 255;
+
+protected:
+    //!发送缓冲区
+    char              *send_packet_ = nullptr;
+    //!接收缓冲区
+    char              *recv_packet_ = nullptr;
+
+    //!DNS 服务器地址
+    zce::Sockaddr_Any   dns_server_addr_;
+
+    //!UDP socket
+    zce::Socket_DataGram    dns_socket_;
+};
+}
---
+++ src/commlib/zcelib/zce/socket/addr_any.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "zce/socket/addr_base.h"
+
+namespace zce
+{
+//Socket地址的通用类，用于有时候，你要用第一个地址类型同时适配in，in6的时候。
+class Sockaddr_Any : public zce::Sockaddr_Base
+{
+public:
+
+    //构造函数，默认初始化为in的地址，使用是要记得改变
+    Sockaddr_Any();
+    //构造函数，根据family确定初始化的类型
+    Sockaddr_Any(int family);
+    //析构函数,内部有virtual函数
+    virtual ~Sockaddr_Any(void);
+
+    //设置sockaddr地址信息,设置成纯虚函数的原因不想让你使用Sockaddr_Any
+    virtual void set_sockaddr(sockaddr* sockaddr_ptr,
+                              socklen_t sockaddr_size) override;
+
+    //通过IP取得域名
+    virtual int getnameinfo(char* host_name,
+                            size_t name_len) const override;
+
+    //通过域名得到IP地址
+    virtual int getaddrinfo(const char* nodename,
+                            uint16_t port_number = 0) override;
+
+    //根据family决定地址类型，然后再通过域名获取IP
+    int getaddrinfo(int family,
+                    const char* node_name,
+                    uint16_t port_number = 0);
+
+    ///设置端口号，
+    virtual void set_port(uint16_t) override;
+    ///取得端口号
+    virtual uint16_t get_port(void) const override;
+
+    //! 设置地址的family
+    void set_family(int family);
+
+    //! 检查地址是否相等
+    bool operator == (const Sockaddr_Any& others) const;
+    //! 检查地址是否不相等
+    bool operator != (const Sockaddr_Any& others) const;
+
+public:
+
+    union
+    {
+        //sockaddr_in 16个字节
+        sockaddr_in  in4_addr_;
+        //sockaddr_in6 28个字节
+        sockaddr_in6 in6_addr_;
+    };
+};
+}
