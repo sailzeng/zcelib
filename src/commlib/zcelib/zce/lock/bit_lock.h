2a3fd30ddb4b2f25cb23ba03ebfe612e519e65e5
sailzeng <fullsail@163.com>
2022-11-29 00:46:17 +0800

    做一个bit lock

---
+++ src/commlib/zcelib/zce/lock/bit_lock.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "zce/shared_mem/mmap.h"
+
+namespace zce
+{
+class bit_lock
+{
+public:
+
+    bit_lock() = default;
+    ~bit_lock() = default;
+
+    //!
+    int open(std::size_t bits_size);
+    //!
+    int open(std::size_t bits_size,
+             const char* file_name,
+             bool if_restore);
+
+    bool test(std::size_t pos);
+
+    //
+    bool lock(std::size_t pos);
+
+    //
+    bool unlock(std::size_t pos);
+
+    //
+    bool try_lock(std::size_t pos);
+
+    //!
+    //constexpr bool operator[](std::size_t pos) const;
+    //!
+    //reference operator[](std::size_t pos);
+
+protected:
+    //!
+    std::size_t bits_size_ = 0;
+    //!
+    zce::shm_mmap shm_mmap_;
+    //!
+    std::atomic_char *bit_lock_ = nullptr;
+};
+}
